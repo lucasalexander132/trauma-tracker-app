ce0d7393e6b759c9f6db9da31544bd835d4a648c
lucasalexander132 <lucasalexander132@gmail.com>
2025-12-04 10:52:21 -0500

    just run the settingsArr function in the component

--- app/(protected)/(tabs)/settings/settingsScreen.tsx
+++ app/(protected)/(tabs)/settings/settingsScreen.tsx
@@ -8,13 +8,12 @@ export default function Settings() {
     const { toggleColorScheme } = useColorScheme();
     const getSettingsArr = useSettingsStore((state) => state.getSettingsArr);
     const setSetting = useSettingsStore((state) => state.setSetting);
-    const settings = getSettingsArr();
     return (
         <View style={theme} className="flex-1 bg-[--color-paper] dark:bg-slate-600 w-full">
             <View className="my-6 mx-8">
                 <SettingSwitch onToggleSwitch={toggleColorScheme} label={"Dark Mode"} />
                 {
-                    settings.map(([key, value]) => <SettingSwitch
+                    getSettingsArr().map(([key, value]) => <SettingSwitch
                         key={key}
                         onToggleSwitchWithSetting={setSetting}
                         settingKey={key as SettingsKeys}
