f508235256b99517e371e2739c878118d692513f
lucasalexander132 <lucasalexander132@gmail.com>
2025-12-04 10:51:10 -0500

    Entry card text and you don't need to useEffect the settings arr

--- app/(protected)/(tabs)/settings/settingsScreen.tsx
+++ app/(protected)/(tabs)/settings/settingsScreen.tsx
@@ -1,22 +1,14 @@
 import theme from "@/assets/styles/theme";
 import SettingSwitch from "@/components/settingSwitch";
-import useSettingsStore, { ISetting, SettingsKeys } from "@/zustand/settingsStore";
-import { isUndefined } from "lodash";
+import useSettingsStore, { SettingsKeys } from "@/zustand/settingsStore";
 import { useColorScheme } from "nativewind";
-import { useEffect, useState } from "react";
 import { View } from "react-native";
 
 export default function Settings() {
     const { toggleColorScheme } = useColorScheme();
     const getSettingsArr = useSettingsStore((state) => state.getSettingsArr);
     const setSetting = useSettingsStore((state) => state.setSetting);
-    const [settings, setSettings] = useState<[string, ISetting][]>([]);
-
-    useEffect(() => {
-        if (!isUndefined(getSettingsArr)) {
-            setSettings(getSettingsArr());
-        }
-    }, [getSettingsArr]);
+    const settings = getSettingsArr();
     return (
         <View style={theme} className="flex-1 bg-[--color-paper] dark:bg-slate-600 w-full">
             <View className="my-6 mx-8">
